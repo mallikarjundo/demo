c376b4f48319e5b75ef8f778014fa1cece72977d
Doddy Mallikarjun <dm00871894@techmahindra.com>
2022-09-27 16:54:25 +0530

    added second controller method

--- src/main/java/com/example/demo/Test2Controller.java
+++ src/main/java/com/example/demo/Test2Controller.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Test2Controller {
 
+	@GetMapping("/ram")
+	public String test() {
+		return "srisri";
+	}
 }
